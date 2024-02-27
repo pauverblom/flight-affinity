7bb7b6800df53aee293776dec03beed24564260c
pauverblom <pauver.mola@gmail.com>
2024-02-27 18:34:04 +0100

    Fixed PlayerEntityMixin methods

--- src/main/java/net/baneina/flightaffinity/mixin/PlayerEntityMixin.java
+++ src/main/java/net/baneina/flightaffinity/mixin/PlayerEntityMixin.java
@@ -1,5 +1,9 @@
 package net.baneina.flightaffinity.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.baneina.flightaffinity.FlightAffinity;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityType;
@@ -15,16 +19,25 @@ import net.baneina.flightaffinity.enchantment.ModEnchantments;
 
 @Mixin(PlayerEntity.class)
 abstract class PlayerEntityMixin extends LivingEntity {
-
-    @Inject(method = "getBlockBreakingSpeed",at = @At("RETURN"),cancellable = true)
-    private void flightAffinity(BlockState block, CallbackInfoReturnable<Float> cir){
-        if (!this.isOnGround() && EnchantmentHelper.getEquipmentLevel(ModEnchantments.FLIGHT_AFFINITY, this) > 0)
-        {
-            cir.setReturnValue(cir.getReturnValue() * 5);
+    @ModifyExpressionValue(method = "getBlockBreakingSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isOnGround()Z"))
+    private boolean flightAffinityEnchantmentAndIsOnGround(boolean originalIsOnGround) {
+        if (!originalIsOnGround && EnchantmentHelper.getEquipmentLevel(ModEnchantments.FLIGHT_AFFINITY, this) > 0) {
+            return false; // If player is in the air and has flight affinity, treat as not on ground
         }
-
+        return originalIsOnGround;
     }
 
+    @ModifyReturnValue(method = "getBlockBreakingSpeed", at = @At("RETURN"))
+    private float modifyMiningSpeed(float original, BlockState block)
+    {
+        if (!isOnGround())
+        {
+            original = original * 100;
+            System.out.println("Multiplicado");
+        }
+        System.out.println("Enviado");
+        return original;
+    }
     public PlayerEntityMixin(EntityType<? extends LivingEntity> type, World world) {
         super(type, world);
     }
