6a4cbbf14769ab40287a286d0b6070ee6a66e082
pauverblom <pauver.mola@gmail.com>
2024-08-25 15:44:51 +0200

    Cleaned up my mess of a code

--- src/main/java/net/baneina/flightaffinity/mixin/PlayerEntityMixin.java
+++ src/main/java/net/baneina/flightaffinity/mixin/PlayerEntityMixin.java
@@ -10,17 +10,17 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = PlayerEntity.class, priority = 2000)
+@Mixin(value = PlayerEntity.class)
 abstract class PlayerEntityMixin extends LivingEntity {
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
-    @ModifyExpressionValue(method = "getBlockBreakingSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isOnGround()Z"))
-    public boolean flightAffinityEnchantmentAndIsOnGround(boolean originalIsOnGround) {
-        boolean returnValue = originalIsOnGround;
-        if (!originalIsOnGround && EnchantmentHelper.getEquipmentLevel(ModEnchantments.FLIGHT_AFFINITY, this) > 0) {
-            returnValue = true; // If player is in the air and has flight affinity, treat as on ground
-        }
-        return returnValue;
+    @ModifyExpressionValue(
+            method = "getBlockBreakingSpeed",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isOnGround()Z")
+    )
+    public boolean shouldTreatAsOnGroundDueToFlightAffinity(boolean trueIsOnGround) {
+        // If player is in the air and has flight affinity, treat as on ground (return true)
+        return trueIsOnGround || EnchantmentHelper.getEquipmentLevel(ModEnchantments.FLIGHT_AFFINITY, this) > 0;
     }
 }
